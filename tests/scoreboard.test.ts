8fe8f0ddb3139b81402daf94bcba18238530f9bc
Wolfgang Koller <w.koller@senegate.at>
2024-06-09 17:42:13 +0200

    Pure Requirements based test-cases setup

---
+++ tests/scoreboard.test.ts
@@ -0,0 +1,32 @@
+/**
+ * Starting point for test case definition, follows the simple task requirements as outlined in the document
+ */
+describe('Requirements tests', () => {
+    test('1. Start new match', () => {
+    });
+    test('2. Update score', () => {
+    });
+    test('3. Finish match', () => {
+    });
+    test('4. Get summary of matches', () => {
+    });
+});
+
+/**
+ * Tests the code based on the example provided in the task requirements
+ */
+describe('Example tests', () => {
+    test('a. Mexico - Canada', () => {
+    });
+    test('b. Spain - Brazil', () => {
+    });
+    test('c. Germany - France', () => {
+    });
+    test('d. Uruguay - Italy', () => {
+    });
+    test('e. Argentina - Australia', () => {
+    });
+    test('Scoreboard check', () => {
+    });
+});
+
